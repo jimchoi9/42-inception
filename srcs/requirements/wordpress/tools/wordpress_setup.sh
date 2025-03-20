a35c00d41244c254c4fd552b79a77ac38a8cb6e0
jiminChoi <jimchoi@student.42seoul.co.kr>
2025-03-20 17:09:25 +0900

    volume

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-  wp core download --path=./wordpress --allow-root
+cd /var/www/html/
 
-  cd wordpress
+  wp core download --allow-root
+
 
 #   # wp-config.php 생성
   wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DB_HOST --skip-check --allow-root
@@ -10,7 +11,7 @@
 #   # WordPress 설치
   wp core install --url=$DOMAIN_NAME --title="inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
   
-chown -R www-data:www-data /wordpress
+# chown -R www-data:www-data /wordpress
 
 # wp user create guest ${WP_EMAIL} --role=$WP_USER --user_pass=$WP_PASSWORD --allow-root
 wp user create $WP_USER  user@test.com --role=contributor --user_pass=$WP_PASSWORD --allow-root
